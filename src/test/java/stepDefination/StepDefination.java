ee8fec288c15bd4f33bfbe2ff9057763701b17e8
azadpratapsingh <iamcoolazad@gmail.com>
2024-04-29 18:05:42 +0530

    Branch name changes from Lecture87 to Lecture85

--- src/test/java/stepDefination/StepDefination.java
+++ src/test/java/stepDefination/StepDefination.java
@@ -3,9 +3,6 @@ package stepDefination;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals; // import static explicitly, wont show by eclipse
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -17,8 +14,6 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import pojo.AddPlace;
-import pojo.Locations;
 import resources.TestDataBuild;
 
 public class StepDefination {
@@ -30,22 +25,6 @@ public class StepDefination {
 	@Given("Add place payload")
 	public void add_place_payload() {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		AddPlace place = new AddPlace();
-		place.setAccuracy(50);
-		place.setAddress("29, side layout, cohen 09");
-		place.setLanguage("French-IN");
-		place.setPhone_number("4564554");
-		place.setWebsite("http://google.com");
-		place.setName("Frontline house");
-		List<String> myList = new ArrayList<String>();
-		myList.add("shoe park");
-		myList.add("shop");
-
-		place.setTypes(myList);
-		Locations location = new Locations();
-		location.setLng(33.427362);
-		location.setLat(-38.383494);
-		place.setLocation(location);
 
 		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
 				.addQueryParam("key", "qaclick123").setContentType(ContentType.JSON).build();
